64eabe4ef38df237a8800b8e39e93572dec62820
eascend <evan.almeidaf@gmail.com>
2018-07-15 14:12:12 -0400

    Fixed small quantity/individual price bug

--- models/order.js
+++ models/order.js
@@ -8,6 +8,7 @@ module.exports = function Order(drinks){
 
     this.add = function(drink, id){
         var storedDrink;
+        var temp = false;
         if(this.totalQty==0){
             storedDrink = this.drinks[id];
             storedDrink = this.drinks[id] = {drink: drink, quantity: 0, price: 0};
@@ -16,19 +17,19 @@ module.exports = function Order(drinks){
             for(var key in this.drinks){
                 if((this.drinks[key].drink.name==drink.name) && (JSON.stringify(this.drinks[key].drink.modifiers) === JSON.stringify(drink.modifiers))){
                     storedDrink = this.drinks[key];
-                    break;
-                }
-                else{
-                    storedDrink = this.drinks[id];
-                    storedDrink = this.drinks[id] = {drink: drink, quantity: 0, price: 0};
+                    temp = true;
                     break;
                 }
             }
+            if(temp==false){
+                storedDrink = this.drinks[id];
+                storedDrink = this.drinks[id] = {drink: drink, quantity: 0, price: 0};
+            }
         }
+            console.log(storedDrink);
             storedDrink.quantity++;
             this.totalPrice+=parseFloat(storedDrink.drink.price);
-            storedDrink.drink.price = storedDrink.drink.price * storedDrink.quantity;
-            console.log(storedDrink.price);
+            storedDrink.price = storedDrink.drink.price * storedDrink.quantity;
             this.totalQty++;
             this.totalPrice = math.round(this.totalPrice, 2); //try to found another built-in function and uninstall mathjs
     };
